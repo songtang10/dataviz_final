b201477da150e680e03a93fc5e9d4b42a307fc55
songtang10 <49631557+songtang10@users.noreply.github.com>
2021-11-30 19:45:13 -0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+new WOW().init();
+  
+function reveal() {
+    console.log("clicked")
+    document.getElementById("hidden").style.display = "flex"; 
+    document.getElementById("clickbutton").style.visibility = "hidden";
+}
